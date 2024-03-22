3b388fcf51fb6a4a9ce1119b5b5fdb0a66a241df
Engreyxien <engreyxien@gmail.com>
2024-03-22 17:21:32 +0800

    php_activity1

--- q3.php
+++ q3.php
@@ -9,17 +9,21 @@
     <h1>Question 3</h1>
     <?php
     function countOnes($arr) {
-      $count = 0;
-      foreach ($arr as $innerArr) {
-        $count += array_count_values($innerArr)[1] ?? 0;
-      }
-      return $count;
+        $count = 0;
+        foreach ($arr as $innerArr) {
+            foreach ($innerArr as $num) {
+                if ($num === 7) {
+                    $count++;
+                }
+            }
+        }
+        return $count;
     }
     
     $numArr = [
-      [1,7,8,9],
-      [1,3,1,1,1,6],
-      [6,20,1,5,1,22]
+        [1, 7, 8, 9],
+        [1, 3, 1, 1, 1, 6],
+        [6, 20, 1, 5, 1, 22]
     ];
     
     $result = countOnes($numArr);
